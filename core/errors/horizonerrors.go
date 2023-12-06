e14b817f01c27d157d2999d38b7f93314b35b685
xu.zhu <xu.zhu.work@outlook.com>
2023-12-06 22:41:35 +0800

    feat: admission validating webhook
    Signed-off-by: xu.zhu <xu.zhu.work@outlook.com>

--- core/errors/horizonerrors.go
+++ core/errors/horizonerrors.go
@@ -209,7 +209,7 @@ var (
 	ErrGenerateRandomID = errors.New("failed to generate random id")
 	ErrDisabled         = errors.New("entity is disabled")
 	ErrDuplicatedKey    = errors.New("duplicated keys")
-	// ErrInternal = errors.New("internal error")
+	ErrValidatingFailed = errors.New("validating failed")
 
 	// http
 	ErrHTTPRespNotAsExpected = errors.New("http response is not as expected")
---
+++ core/middleware/admission/admission.go
@@ -0,0 +1,56 @@
+package admission
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/horizoncd/horizon/core/common"
+	"github.com/horizoncd/horizon/core/middleware"
+	admissionwebhook "github.com/horizoncd/horizon/pkg/admission"
+	admissionmodels "github.com/horizoncd/horizon/pkg/admission/models"
+	"github.com/horizoncd/horizon/pkg/auth"
+	"github.com/horizoncd/horizon/pkg/server/response"
+	"github.com/horizoncd/horizon/pkg/server/rpcerror"
+)
+
+// Middleware to validate and mutate admission request
+func Middleware(skippers ...middleware.Skipper) gin.HandlerFunc {
+	return middleware.New(func(c *gin.Context) {
+		// get auth record
+		record, ok := c.Get(common.ContextAuthRecord)
+		if !ok {
+			response.AbortWithRPCError(c,
+				rpcerror.BadRequestError.WithErrMsg("request with no auth record"))
+			return
+		}
+		attr := record.(auth.AttributesRecord)
+		// non resource request or read only request should be ignored
+		if !attr.IsResourceRequest() || attr.IsReadOnly() {
+			c.Next()
+			return
+		}
+		resource := attr.GetResource()
+		if len(attr.GetSubResource()) > 0 {
+			resource = fmt.Sprintf("%s/%s", resource, attr.GetSubResource())
+		}
+		var object interface{}
+		if err := c.ShouldBind(object); err != nil {
+			response.AbortWithRPCError(c,
+				rpcerror.ParamError.WithErrMsg(fmt.Sprintf("request body is invalid, err: %v", err)))
+			return
+		}
+		admissionRequest := &admissionwebhook.Request{
+			Operation: admissionmodels.Operation(attr.GetVerb()),
+			Resource:  resource,
+			Version:   attr.APIVersion,
+			Object:    object,
+			OldObject: nil,
+		}
+		if err := admissionwebhook.Validating(c, admissionRequest); err != nil {
+			response.AbortWithRPCError(c,
+				rpcerror.ParamError.WithErrMsg(fmt.Sprintf("admission validating failed: %v", err)))
+			return
+		}
+		c.Next()
+	}, skippers...)
+}
